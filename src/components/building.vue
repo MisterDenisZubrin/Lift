80ae89fd8b3de38a8620fc90750ad60fdd58a8cb
Denis <dendel1998@gmail.com>
2022-06-29 19:10:37 +0300

    Созданы компоненты для сборки лифта

---
+++ src/components/building.vue
@@ -0,0 +1,15 @@
+<!-- Здание является конфигурационным файлом. -->
+<!-- Здесь задается количество этажей и лифтов -->
+<template >
+  <div>
+    
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+  
+</style>
